2deef27cfd3beecb83f8a41c474e84120f5cd4e2
sanchez <sanchez@sanchez-VirtualBox>
2013-10-05 03:17:28 +0200

    link the lib gmp to the project install the GMP-lib  in  : "/usr/local/bin" and they include file in : "/usr/local/include"

--- test.c
+++ test.c
@@ -7,12 +7,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <gmp.h>
 
-int main(int argc, char **argv) {
-	printf("start");
-	printf("test first commit");
-	printf("modif 1");
-	printf("end");
-	return 0;
+int main(void)
+{
+	 mpz_t x;
+	 mpz_t y;
+	 mpz_t result;
+
+	 mpz_init(x);
+	 mpz_init(y);
+	 mpz_init(result);
+
+	 mpz_set_str(x, "7612058254738945", 10);
+	 mpz_set_str(y, "9263591128439081", 10);
+
+	 mpz_mul(result, x, y);
+	 gmp_printf("\n    %Zd\n*\n    %Zd\n--------------------\n%Zd\n\n", x, y, result);
+
+	 /* free used memory */
+	 mpz_clear(x);
+	 mpz_clear(y);
+	 mpz_clear(result);
+	 return EXIT_SUCCESS;
 }
 

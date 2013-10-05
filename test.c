a9faa404aa1e73af8ba1f6cf6066082bfa7ab85a
sanchez <sanchez@sanchez-VirtualBox>
2013-10-05 03:41:17 +0200

    modif

--- test.c
+++ test.c
@@ -14,17 +14,22 @@ int main(void)
 	 mpz_t x;
 	 mpz_t y;
 	 mpz_t result;
+	 mpz_t result_cp;
 
 	 mpz_init(x);
 	 mpz_init(y);
 	 mpz_init(result);
+	 mpz_init(result_cp);
 
-	 mpz_set_str(x, "7612058254738945", 10);
-	 mpz_set_str(y, "9263591128439081", 10);
+	 mpz_set_str(x, "412650877539559062731483450512926855", 10);
+	 mpz_set_str(y, "3822629008318060962512312671273578748225608876420255", 10);
 
 	 mpz_mul(result, x, y);
+
 	 gmp_printf("\n    %Zd\n*\n    %Zd\n--------------------\n%Zd\n\n", x, y, result);
 
+	 mpz_add(result_cp, result, x);
+	 gmp_printf("\n%Zd\n",result_cp);
 	 /* free used memory */
 	 mpz_clear(x);
 	 mpz_clear(y);

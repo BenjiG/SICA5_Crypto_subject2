7230b8a23aebe6b09f5447e70c3c9e9570c8d1fc
sanchez <sanchez@sanchez-VirtualBox>
2013-10-05 02:43:05 +0200

    first build test include and dependency

---
+++ test.c
@@ -0,0 +1,15 @@
+/*
+ * test.c
+ *
+ *  Created on: 5 oct. 2013
+ *      Author: sanchez
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	printf("test first commit");
+	return 0;
+}
+
